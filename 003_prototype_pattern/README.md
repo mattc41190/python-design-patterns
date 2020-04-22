019b26f618ce64400b629406d6d55e52102574ae
Matt Cale <matt.cale@opcity.com>
2020-04-22 09:00:13 -0500

    feat: prototype pattern discussed

--- 003_prototype_pattern/README.md
+++ 003_prototype_pattern/README.md
@@ -6,4 +6,23 @@ This is a demonstration of the prototype object creation pattern in Python.
 
 the prototype pattern is used to create objects based on other objects. I.e. cloning objects.
 
+Using a deeply copied prototype pattern we can maintain a chain of clones while not affecting the prototype. 
+
+A good example is recipes sharing software.
+
+Ex:
+
+- Bob finds a recipe on recipes.com
+- He makes it and decides it can be improved 
+- He copies recipe, maintaining a reference to the original recipe while at the same time so that the original can always be found even as it independently evolves over time.
+- He modifies his local copy and loves the new flavor
+- Sarah finds Bobs recipe, decides _it_ can be improved and copies it.
+- Now Sarah's recipes refs Bob's, and Bob's ref the original; however all three are still allowed to grow independently of each other.
+
+__Note: Maintaining reference is something I did to demonstrate the relationship between the prototypes. IT is not required. All that is required is that the object be copied either deeply or shallowly.__
+
 ## How do I use this?
+
+- Read the code in `main.py`
+- Run the code in `main.py` (`python main.py`) 
+- Observe output
--- 003_prototype_pattern/main.py
+++ 003_prototype_pattern/main.py
@@ -1,3 +1,4 @@
+import copy
 from collections import OrderedDict
 
 class Book(object):
@@ -20,6 +21,55 @@ class Book(object):
             attrs.append('\n')
         return ''.join(attrs)
 
-my_book = Book('Silver Chair', 'C.S. Lewis', 15, publisher='harper')
+class Prototype(object):
+    def __init__(self):
+        self.objects = dict()
+    
+    def register(self, identifier, obj):
+        self.objects[identifier] = obj
+    
+    def deregister(self, identifier):
+        del self.objects[identifier]
+    
+    def clone(self, identifier, **attr):
+        found = self.objects.get(identifier)
+        if not found:
+            raise ValueError('Not object found for identifier: {}'.format(identifier))
+        obj = copy.deepcopy(found)
+        obj.__dict__.update(attr)
+        return obj
 
-print(my_book)
+def main():
+    prototype = Prototype()
+
+    b1 = Book(
+        'The C Programming Language', 
+        ('Brian W. Kernighan','Dennis M.Ritchie'), 
+        price=118, 
+        publisher='Prentice Hall', 
+        length=228, 
+        publication_date='1978-02-22', 
+        tags=('C', 'programming', 'algorithms', 'data structures')
+    )
+    
+    cid = 'k&r-first'
+
+    prototype.register(cid, b1)
+
+    b2 = prototype.clone(
+        cid, 
+        name='The C Programming Language (ANSI)', 
+        price=48.99, 
+        length=274, 
+        publication_date='1988-04-01', 
+        edition=2
+    )
+
+    for i in (b1, b2):
+        print(i)
+    
+    print('ID b1: {} != ID b2: {}'.format(id(b1), id(b2)))
+
+
+if __name__ == '__main__':
+    main()
